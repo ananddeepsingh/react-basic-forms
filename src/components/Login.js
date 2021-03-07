ef4fc6e6f25c81d5a5600716fd61a03669c9908d
ananddeepsingh <singh.ananddeep@gmail.com>
2021-03-07 17:40:26 +0800

    Updating logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,33 +11,29 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const isValidForm = formValidation();
-    
-    if(isValidForm){
-      setIsFormValid(false);
-    }
-    return false;
+    alert("form submitted")
   }
 
   const formValidation = () => {
     const userNameError = {};
     const passwordError = {};
     const confirmPasswordError = {};
-    let isValid = true;
+    let isValid = false;
 
-    if(userName.trim().length < 8){
+    if(userName.length === 0 || userName.trim().length < 8){
       userNameError.userNameShort = "Please enter more than 8 characters";
-      isValid = false;
     }
-
-    if(password.trim().length < 8){
+    
+    if(password.length === 0 || password.trim().length < 8){
       passwordError.passwordShort = "Please enter more than 8 characters";
-      isValid = false;
+    }
+    
+    if(password.length === 0 || confirmPassword === 0 || password !== confirmPassword){
+      confirmPasswordError.confirmPasswordShort = "Password Mismatch. Please check.";
     }
 
-    if(password !== confirmPassword){
-      confirmPasswordError.confirmPasswordShort = "Password Mismatch. Please check.";
-      isValid = false;
+    if(userName.trim().length > 7 && password.trim().length > 7 && password === confirmPassword){
+      isValid = true;
     }
 
     setuserNameError(userNameError);
@@ -48,31 +44,38 @@ const Login = () => {
 
   const renderErrors = (obj) => {
     const html = [];
-    {Object.keys(obj).map( (key, i) => {
-      html.push( <div key={i} className="error">{obj[key]}</div>);
-    })}
+    {
+      Object.keys(obj).map( (key, i) => {
+        html.push( <div key={i} className="error">{obj[key]}</div>);
+      })
+    }
     return html
+  }
+
+  const isFormButtonEnabled = (e) => {
+    const isValidForm = formValidation();
+    isValidForm ? setIsFormValid(true) : setIsFormValid(false)
   }
 
   return(
     <form onSubmit={onSubmit}>
       <div className="row">
         <label>Username</label>
-        <input type="text" value={userName} onChange={(e) => {setUserName(e.target.value)}} placeholder="Username" name="username" />
+        <input type="text" value={userName} onChange={(e) => {setUserName(e.target.value)}} onBlur={isFormButtonEnabled} placeholder="Username" name="username" />
         {renderErrors(userNameError)}
       </div>
       <div className="row">
         <label>Password</label>
-        <input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} placeholder="Password" name="password" />  
+        <input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} onBlur={isFormButtonEnabled} placeholder="Password" name="password" />  
         {renderErrors(passwordError)}
       </div>
       <div className="row">
         <label>Confirm Password</label>
-        <input type="password" value={confirmPassword} onChange={(e) => {setConfirmPassword(e.target.value)}} placeholder="Confirm Password" name="confirmPassword" />  
+        <input type="password" value={confirmPassword} onChange={(e) => {setConfirmPassword(e.target.value)}} onBlur={isFormButtonEnabled} placeholder="Confirm Password" name="confirmPassword" />  
         {renderErrors(confirmPasswordError)}
       </div>
       <div className="row">
-        <button type="submit" id="submit">Submit</button>
+        <button type="submit" id="submit" disabled={!isFormValid}>Submit</button>
       </div>
     </form>
   )
